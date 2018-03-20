dd784a571c7b9fb1ec7b4673781b877ec2630230
Marcel Candido <phpmarcel@gmail.com>
2018-03-19 22:28:31 -0300

    Ajustes para funcionamento regular do módulo ZF2

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,13 @@
 <?php
 
-return [];
+return [
+
+    'service_manaer' => [
+        'factories' => [
+            'ElephantIO\Service\SocketIOService' => function ($sm) {
+                return new \ElephantIO\Service\SocketIOService($sm);
+            },
+        ],
+    ],
+
+];
