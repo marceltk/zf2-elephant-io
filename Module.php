34c529c51234a1e11f84ee17fa154f12cf62fdd5
Marcel Candido <phpmarcel@gmail.com>
2018-03-19 21:40:36 -0300

    Ajustes gerais para correto funcionamento

--- Module.php
+++ Module.php
@@ -6,33 +6,38 @@ use Zend\ModuleManager\ModuleManagerInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\EventManager\EventInterface;
 
-class Module {
+class Module
+{
 
-    public function onBootstrap(EventInterface $e) {
-
+    public function onBootstrap(EventInterface $e)
+    {
     }
 
-    public function getAutoloaderConfig() {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__."/src/",
-                ),
-            )
-        );
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . "/src/",
+                ],
+            ],
+        ];
     }
 
-    public function getConfig(){
-       return include __DIR__ . '/config/module.config.php';
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getServiceConfig(){
-        return array(
-            'factories' => array(
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
 
-            ),
-        );
+            ],
+        ];
     }
- 
+
 }
+
 ?>
